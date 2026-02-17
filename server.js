f22d0a6d2c5b95d18955dea5abd5519fffd7645f
eric-deau <edeau@my.bcit.ca>
2026-02-16 20:58:34 -0800

    Fixed hosting bug

--- server.js
+++ server.js
@@ -109,6 +109,6 @@ async function verifyPassword(password, hash) {
 }
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
